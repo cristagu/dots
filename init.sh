1ec731a9a6e95be7ccb4a63f5fc3284a04cb5e35
Cristian Neagu | eMAG, Technology <cristian.neagu@emag.ro>
2019-03-05 16:06:08 +0200

    init.sh edited online with Bitbucket

--- init.sh
+++ init.sh
@@ -1,5 +1,14 @@
-#!/bin/bash
-git init --bare $HOME/.cfg
-alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
-config config --local status.showUntrackedFiles no
-echo "alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'" >> $HOME/.bashrc
+git clone --bare ssh://git@stash.emag.network/ntn/dots-cristi.git $HOME/.cfg
+function config {
+   /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
+}
+mkdir -p .config-backup
+config checkout
+if [ $? = 0 ]; then
+  echo "Checked out config.";
+  else
+    echo "Backing up pre-existing dot files.";
+    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+fi;
+config checkout
+config config status.showUntrackedFiles no
